7d5c2a72eb3c0c2e03414b791bb91e37da42be9f
martinSabatini <146122086+martinSabatini@users.noreply.github.com>
2024-12-11 23:34:13 +0000

    ExcuseGenratorFInalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import "./index.html";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let a = who[Math.floor(Math.random() * who.length)];
+  let b = action[Math.floor(Math.random() * action.length)];
+  let c = what[Math.floor(Math.random() * what.length)];
+  let d = when[Math.floor(Math.random() * when.length)];
+
+  document.getElementById(
+    "excuse"
+  ).innerHTML = `OMG you will not believe me but... 
+  <p> ${a} ${b} ${c} ${d}.</p>`;
+  const button = document.getElementById("refresfhPage");
+  button.addEventListener("click", function() {
+    location.reload();
+  });
 };
